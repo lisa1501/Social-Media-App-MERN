58aba4985b3b0253650834d64de3469bc78cd690
halisa <halisasaipulaa@gmail.com>
2023-02-22 15:05:01 -0800

    implemented update post by id

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import useStyles from './styles';
 import moment from 'moment';
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     console.log(moment(post.createAt).fromNow())
 
     const classes = useStyles();
@@ -19,15 +19,16 @@ const Post = ({ post }) => {
                 <Typography variant='body2'>{moment(post.createAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color:'white'}} size="small" onClick={()=>{}}>
+                <Button style={{color:'white'}} size="small" onClick={()=>setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize='default'/>
                 </Button>
             </div>
             <div className={classes.details}>
                 <Typography variant='body2' color="textSecondary">{post.tags.map((tag) =>`#${tag} `)}</Typography>
-            </div>
+            </div> 
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.CardActions}>
                 <Button size="small" color="primary" onClick={()=>{}}>
