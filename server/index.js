ff29749acbde83a6c783f684bd22b40d73894048
halisa <halisasaipulaa@gmail.com>
2023-02-22 10:38:37 -0800

    fixed the network issue-blocked by CORS Policy

--- server/index.js
+++ server/index.js
@@ -9,13 +9,11 @@ dotenv.config();
 
 const app = express();
 
-app.use('/posts',postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-
+app.use('/posts',postRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URL;
 
@@ -23,8 +21,8 @@ const PORT = process.env.PORT|| 8080;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
 
 
 
