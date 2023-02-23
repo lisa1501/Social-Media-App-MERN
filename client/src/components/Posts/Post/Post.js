dab384296f9205433041a8c1db97c776803bc928
halisa <halisasaipulaa@gmail.com>
2023-02-22 16:17:51 -0800

    implemented delete post from client site

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -3,17 +3,21 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
-import moment from 'moment';
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
-    console.log(moment(post.createAt).fromNow())
-
+    
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} img={post.selectedFile} title={post.title}/>
+            <CardMedia className={classes.media} img={post.selectedFile 
+            || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'}  title={post.title}/>
             <div className={classes.overlay}>
                 <Typography variant='h6'>{post.creator}</Typography>
                 <Typography variant='body2'>{moment(post.createAt).fromNow()}</Typography>
@@ -36,7 +40,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like 
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small'/>
                     Delete
                 </Button>
