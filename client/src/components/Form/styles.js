ec9878a40ff90edfe3b2760f59935841c34c2b3e
halisa <halisasaipulaa@gmail.com>
2023-02-23 08:12:04 -0800

    styled Post,Posts and Form components

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -1,18 +1,25 @@
+
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
-    appBar: {
-        borderRadius: 15,
-        margin: '30px 0',
+export default makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+    },
+    },
+    paper: {
+        padding: theme.spacing(2),
+    },
+    form: {
         display: 'flex',
-        flexDirection: 'row',
+        flexWrap: 'wrap',
         justifyContent: 'center',
-        alignItems: 'center',
     },
-    heading: {
-        color: 'rgba(0,183,255, 1)',
+    fileInput: {
+        width: '97%',
+        margin: '10px 0',
     },
-    image: {
-        marginLeft: '15px',
+    buttonSubmit: {
+        marginBottom: 10,
     },
 }));
--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -1,18 +1,52 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
-    appBar: {
-        borderRadius: 15,
-        margin: '30px 0',
+export default makeStyles({
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundBlendMode: 'darken',
+    },
+    border: {
+        border: 'solid',
+    },
+    fullHeightCard: {
+        height: '100%',
+    },
+    card: {
         display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        borderRadius: '15px',
+        height: '100%',
+        position: 'relative',
     },
-    heading: {
-        color: 'rgba(0,183,255, 1)',
+    overlay: {
+        position: 'absolute',
+        top: '20px',
+        left: '20px',
+        color: 'white',
     },
-    image: {
-        marginLeft: '15px',
+    overlay2: {
+        position: 'absolute',
+        top: '20px',
+        right: '20px',
+        color: 'white',
     },
-}));
+    grid: {
+        display: 'flex',
+    },
+    details: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '20px',
+    },
+    title: {
+        padding: '0 16px',
+    },
+    cardActions: {
+        padding: '0 16px 8px 16px',
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+});
