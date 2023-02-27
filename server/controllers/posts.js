eb235e118313874b50a9a6fed74d8945b4cfcb30
halisa <halisasaipulaa@gmail.com>
2023-02-27 15:57:56 -0800

    implemented pagination feature fully functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,20 +2,26 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req,res) => {
+    const { page } = req.query;
     try {
-        const postMessage = await PostMessage.find();
+        const LIMIT = 4;
+        const startIndex = (Number(page)-1) * LIMIT;
+        const total = await PostMessage.countDocuments({});
+        const posts = await  PostMessage.find()
+                                        .sort({ _id: -1})
+                                        .limit(LIMIT)
+                                        .skip(startIndex);
 
-        res.status(200).json(postMessage)
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)})
     }catch(error){
         res.status(404).json({ message : error.message })
     }
     
 }
 
+//QUERY -> /posts?page=1 -> page=1
+//PARAMS -> /posts/123 -> id=123
 export const getPostsBySearch = async(req, res) =>{
-
-    //QUERY -> /posts?page=1 -> page=1
-    //PARAMS -> /posts/123 -> id=123
 
     const { searchQuery, tags } = req.query;
     try{
