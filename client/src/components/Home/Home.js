2b345ade28da405d27247f37b454aab1524dbfc7
halisa <halisasaipulaa@gmail.com>
2023-02-27 10:18:44 -0800

    implementd pagination feature

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React,{ useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
-import { getPosts } from '../../actions/posts'
+import { getPosts } from '../../actions/posts';
+import Pagination from '../Pagination';
+import { mergeClasses } from '@material-ui/styles';
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(0);
@@ -23,6 +25,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper  elevation={1}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -23,8 +23,6 @@ const Navbar = () => {
         dispatch({ type:'LOGOUT'});
         navigate.push('/');
         setUser(null);
-    
-
     }
 
     useEffect(() => {   
