421a00504965fee93048558c9c65ab02e8e1601a
halisa <halisasaipulaa@gmail.com>
2023-02-27 13:42:15 -0800

    implemented get post by search

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -14,6 +14,16 @@ export const getPosts = () => async (dispatch) => {
     }
 }
 
+export const getPostsBySearch = (searchQuery) => async (dispatch) =>{
+    try{
+        const { data: {data} } = await api.fetchPostsBySearch(searchQuery);
+        
+        console.log(data)
+    }catch(error){
+        console.log(error)
+    }
+}
+
 export const createPost = (post) => async (dispatch) => {
 
     try{
