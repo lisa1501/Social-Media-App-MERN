2368c92f3945829a1a92307c1abbf12419c82634
halisa <halisasaipulaa@gmail.com>
2023-02-27 16:20:29 -0800

    rendered page loading

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,19 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, START_LOADING, END_LOADING } from '../constants/actionTypes';
 
-export default (state = [], action) =>{
+export default (state = { isLoading: true, posts: [] }, action) =>{
     switch (action.type) {
+        case START_LOADING :
+            return { ...state, isLoading: true}
+
+        case END_LOADING:
+            return { ...state, isLoading: false}
+
         case DELETE:
-            return state.filter((post) => post._id !== action.payload);
+            return { ...state, posts:state.posts.filter((post) => post._id !== action.payload)};
 
         case UPDATE:
         case LIKE:
-            return state.map((post) => post._id === action.payload._id ? action.payload : post);
+            return { ...state, posts:state.posts.map((post) => post._id === action.payload._id ? action.payload : post)};
 
         case FETCH_ALL:
             return{
@@ -21,7 +27,7 @@ export default (state = [], action) =>{
             return { ...state, posts:action.payload};
 
         case CREATE:
-            return [...state, action.payload];
+            return { ...state, posts: [ ...state.posts,action.payload]};
     
         default:
             return state;
