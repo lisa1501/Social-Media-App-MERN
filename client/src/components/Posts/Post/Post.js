72a7008ca21bccbd2de80c36228be906b078c2da
halisa <halisasaipulaa@gmail.com>
2023-02-28 19:44:01 -0800

    improve the like rendering fast

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
@@ -17,16 +17,29 @@ const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
     const history = useHistory();
-    // console.log(user.result._id)
-    // console.log(post.creator)
+    const [likes, setLikes] = useState(post?.likes);
+    
+    const userId = user?.result.sub || user?.result?._id
+    const hasLikedPost =likes.find((like) => like === userId)
+
+    const handleLike = async () =>{
+        dispatch(likePost(post._id));
+
+        if(hasLikedPost){
+            setLikes(likes.filter((id) => id !== userId));
+        }else{
+            setLikes([ ...likes, userId]);
+        }
+
+    }
 
     const Likes = () => {
-        if (post.likes.length > 0) {
-            return post.likes.find((like) => like === (user?.result?.sub || user?.result?._id))
+        if (likes.length > 0) {
+            return likes.find((like) => like === userId)
                 ? (
-                <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+                <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length >= 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}` }</>
                 ) : (
-                <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
             }
         
@@ -65,11 +78,11 @@ const Post = ({ post, setCurrentId }) => {
             </CardContent>
             </ButtonBase>
             <CardActions className={classes.CardActions}>
-                <Button size="small" color="primary" disabled={!user?.result} onClick={()=>dispatch(likePost(post._id))}>
+                <Button size="small" color="primary" disabled={!user?.result} onClick={handleLike}>
                     <Likes />
                 </Button>
                 {(user?.result?.sub === post?.creator ||user?.result?._id === post?.creator) && (
-                    <Button size="small" color="primary" onClick={()=>dispatch(deletePost(post._id))}>
+                    <Button size="small" color="secondary" onClick={()=>dispatch(deletePost(post._id))}>
                         <DeleteIcon fontSize='small'/>
                         Delete
                     </Button>
